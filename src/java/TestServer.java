5a7aab3d861956b7fe8a2ef953d47a7bc8ff818e
eruelas <cyberruelas@gmail.com>
2019-10-08 22:07:37 -0700

    This version read a file and give the content

--- src/java/TestServer.java
+++ src/java/TestServer.java
@@ -2,7 +2,11 @@ public class TestServer {
     public static void main(String[] args) {
         Servidor servidor = new Servidor(9001);
         String msj = servidor.mensajeEntrante();
-        System.out.println(msj);
-
+        ReadFile readFile = new ReadFile();
+        if(msj.compareTo("readFile")==0){
+            readFile.readFile();
+        }else {
+            System.out.println(msj);
+        }
     }
 }
